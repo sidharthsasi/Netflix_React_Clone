bc49a34f4274d83847499c25a02e6ee90c2d64d7
Sidharth Sasi <sidharthsasi04@gmail.com>
2022-08-17 15:36:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import  './App.css';
+import NavBar from "./Components/NavBar/NavBar";
+import {action,comedy,horror,originals, romance} from './urls'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+        <Banner/>
+        <RowPost url={originals} title='Netflix Originals'/>
+        <RowPost url={action} title='Action' isSmall/>
+        <RowPost url={romance} title='Romance' isSmall/>
+        <RowPost url={horror} title='Horror' isSmall/>
+        <RowPost url={comedy} title='Comedy' isSmall/>
+    </div>
+  );
+}
+
+export default App;
